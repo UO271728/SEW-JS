df9c77abd1c7ab6e6f7b6f7c164162f5f7bbd59a
Rafael Muñiz <71430118+UO271728@users.noreply.github.com>
2022-11-21 19:17:24 +0100

    Add files via upload

--- Ejercicio3/CalculadoraMilan.js
+++ Ejercicio3/CalculadoraMilan.js
@@ -3,126 +3,149 @@ class Calculadora{
 
     constructor (){
         this.memory="";
-        this.operando1="";
+        this.operando1="0";
         this.operando2="";
         this.operator="";
+        this.action="";
+        
         document.addEventListener('keydown',(event) =>{
-            const keyName = event.key;
-
-            if(keyName == "0" || keyName == "1" || keyName == "2" || keyName == "3" || keyName == "4" || keyName == "5" || keyName == "6" || keyName == "7" || keyName == "8" || keyName == "9"){
-                this.digitos(keyName);
-            }
-            else if(keyName == "+"){
-                this.suma();
-            }
-            
-            else if(keyName == "-"){
-                this.resta();
-            }
-
-            else if(keyName == "*"){
-                this.multiplicacion();
-            }
-
-            else if(keyName == "/"){
-                this.division();
-            }
-
-            else if(keyName == "."){
-                this.punto();
-            }
-
-            else if (keyName =="%"){
-                this.porcentaje();
-            }
-
-            else if(keyName == "r"){
-                this.raiz();
-            }
-
-            else if(keyName == "="){
-                this.igual();
-            }
-
-            else if(keyName == "Escape"){
-                this.on();
-            }
-
-            
+            this.registrarTecla(event.key);
         });
     }
 
-    digitos(numero){
-        if(this.operator!=""){
-            if(this.operando2[0] == '0'){
-                this.operando2 = numero;
-            }
-            else{
-                this.operando2+= numero;
-            }
-            document.getElementById("pantalla").value = this.operando2;
+    registrarTecla(tecla){
+        if(tecla == "0" || tecla == "1" || tecla == "2" || tecla == "3" || tecla == "4" || tecla == "5" || tecla == "6" || tecla == "7" || tecla == "8" || tecla == "9"){
+            this.digitos(tecla);
         }
-        else{
-            if(this.operando1[0] == '0'){
-                this.operando1 = numero;
-            }
-            else{
-                this.operando1+= numero;
-            }
-            document.getElementById("pantalla").value = this.operando1;
+        else if(tecla == "+"){
+            this.suma();
+        }
+        
+        else if(tecla == "-"){
+            this.resta();
+        }
+
+        else if(tecla == "*"){
+            this.multiplicacion();
+        }
+
+        else if(tecla == "/"){
+            this.division();
+        }
+
+        else if(tecla == "."){
+            this.punto();
+        }
+
+        else if (tecla =="%"){
+            this.porcentaje();
+        }
+
+        else if(tecla == "r"){
+            this.raiz();
+        }
+
+        else if(tecla == "="){
+            this.igual();
+        }
+
+        else if(tecla == "Escape"){
+            this.on();
+        }
+        else if(tecla == "Enter"){
+            this.igual();
         }
     }
 
+    digitos(numero){
+        if(this.action != "igual"){
+            if(this.operator!=""){
+                if(this.operando2[0] == '0'){
+                    this.operando2 = numero;
+                }
+                else{
+                    this.operando2+= numero;
+                }
+                this.mostrarPantalla(this.operando2);
+            }
+            else{
+                if(this.operando1[0] == '0'){
+                    this.operando1 = numero;
+                }
+                else{
+                    this.operando1+= numero;
+                }
+                this.mostrarPantalla(this.operando1);
+            }
+        }else{
+            this.operando1 = numero;
+            this.operando2 = "";
+            this.operator = "";
+            this.mostrarPantalla(this.operando1);
+        }
+            this.action ="digito";
+
+    }
+
     punto(){
+        this.action="punto"
         if(this.operator!=""){
             if(this.operando2.substring(this.operando2.length -1)!='.'){
                 this.operando2+='.';
-                document.getElementById("pantalla").value = this.operando2;
+                this.mostrarPantalla(this.operando2);
             }
         }
         else{
             if(this.operando1.substring(this.operando1.length -1)!='.'){
                 this.operando1+='.';
-                document.getElementById("pantalla").value = this.operando1;
+               this.mostrarPantalla(this.operando1);
             }
         }
     }
 
     suma(){
-        if(this.operando2!=""){
-            this.igual();
-        }
-        this.operator="+";
-        this.operando2="";
+        this.operacionesBasicas('+');
+        this.action = "suma";
     }
 
     resta(){
-        if(this.operando2!=""){
-            this.igual()
-        }
-        this.operator='-';
-        this.operando2="";
+        this.operacionesBasicas('-');
+        this.action = "resta";
     }
 
     multiplicacion(){
-        if(this.operando2!=""){
-            this.igual()
-        }
-        this.operator='*';
-        this.operando2="";
+        this.operacionesBasicas('*');
+        this.action = "multiplicacion";
     }
 
     division(){
+        this.operacionesBasicas('/');
+        this.action = "division";
+    }
+
+    operacionesBasicas(operador){
         if(this.operando2!=""){
             this.igual();
         }
-        this.operator='/';
+        this.operator=operador;
         this.operando2="";
     }
 
+    mostrarPantalla(cadena){
+        document.getElementById("pantalla").value = cadena;
+    }
+
     mrc(){
-        this.operando1 = this.memory;
-        document.getElementById("pantalla").value = this.operando1;
+        if(this.action == "mrc"){
+            this.mostrarPantalla(this.memory);
+            this.operando1 = "";
+            this.memory = "0";
+        }
+        else{
+            this.action ="mrc";
+            this.operando1 = this.memory;
+            this.mostrarPantalla(this.operando1);
+        }
     }
 
     mMenos(){
@@ -141,6 +164,9 @@ class Calculadora{
 
     igual(){
         try{
+            if(this.action == "digito"){
+                this.action = "igual";
+            }
             if(this.operator =="*" && this.operando2==""){
                 this.operando2 = this.operando1;
             }
@@ -150,7 +176,7 @@ class Calculadora{
             }
             if(this.operando2!=""){
                 var resultado = eval(this.operando1+""+this.operator+""+this.operando2);
-                document.getElementById("pantalla").value = Number(resultado);
+                this.mostrarPantalla(Number(resultado));
                 this.operando1 = resultado;
             }
 
@@ -160,6 +186,7 @@ class Calculadora{
     }
 
     porcentaje(){
+        this.action="porcentaje";
         if(this.operando1!="" && this.operando2!=""){
             var percentage = (Number(this.operando2)/100)*Number(this.operando1);
             this.operando2 = percentage;
@@ -168,15 +195,19 @@ class Calculadora{
     }
 
     raiz(){
-        if(this.operando2!=""){
-            var resultado = Math.sqrt(Number(this.operando2))
-            this.operando2 = resultado;
-            document.getElementById("pantalla").value = resultado;
-        }
-        else{
+        if(this.action=="igual" || this.operando2==""){
             var resultado = Math.sqrt(Number(this.operando1))
             this.operando1 = resultado;
-            document.getElementById("pantalla").value = resultado;
+            this.operator="";
+            this.operando2="";
+            this.mostrarPantalla(resultado);
+        }
+        else{
+            this.action = "raiz";
+            var resultado = Math.sqrt(Number(this.operando2))
+            this.operando2 = resultado;
+            this.mostrarPantalla(resultado);
+            
         }
     }
 
@@ -188,7 +219,7 @@ class Calculadora{
             else{
                 this.operando2 = this.operando2.substring(1,this.operando2.length)
             }
-            document.getElementById("pantalla").value = this.operando2;
+            this.mostrarPantalla(this.operando2);
         }
         else{
             if(Number(this.operando1)>0){
@@ -197,28 +228,27 @@ class Calculadora{
             else{
                 this.operando1 = this.operando1.substring(1,this.operando1.length)
             }
-            document.getElementById("pantalla").value = this.operando1;
+            this.mostrarPantalla(this.operando1);
         }
+        this.action = "cambioSigno";
     }
 
     on(){
         this.operando1 = "";
         this.operator = "";
         this.operando2 = "";
-        document.getElementById("pantalla").value = '0';
+        this.action= "";
+        this.mostrarPantalla('0');
     }
 
     ce(){
         if(this.operando2!=""){
-            this.operando2 = "0"
-            document.getElementById("pantalla").value = this.operando2;
+            this.operando2 = "0";
+            this.mostrarPantalla('0');
         }
         else{
-            this.operando1 = "0";
-            document.getElementById("pantalla").value = this.operando1;
+            this.on();
         }
+        
     }
-
-
-
 }
